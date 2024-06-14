4462bc5fe8b133066dc22cd6de29693760e3b07e
SaxeCoburgGotha <b.a.bernal@outlook.com>
2024-06-14 15:53:15 -0500

    added setup

---
+++ src/data_analysis/setup.py
@@ -0,0 +1,11 @@
+"""
+    Simple setup for Data Analysis
+    Copy right by Benjamin A. Bernal. JUN2024
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="data_analysis",
+    packages=find_packages()
+)
